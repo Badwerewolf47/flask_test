0fb7e7cb7102247c8d6f930969ac5694306c5a04
badwerewolf47 <pudgerikivich@gmail.com>
2024-03-29 10:45:51 +0300

    Дабалены кординаты игркоов

--- app.py
+++ app.py
@@ -14,5 +14,11 @@ def handle_message(message):
     # Отправляем обратно тот же текст сообщения
     emit('response', message)
 
+@socketio.on('move')
+def handle_move(data):
+    # Передаем координаты игрока всем подключенным клиентам, кроме отправителя
+    emit('player_moved', data, broadcast=True, include_self=False)
+
+
 if __name__ == '__main__':
     socketio.run(app, debug=True)
